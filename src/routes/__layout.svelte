4b064053072724baf27c02fb7378738087365dc6
coldino <28345893+coldino@users.noreply.github.com>
2022-02-11 15:59:36 +0000

    feat: install windicss

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import 'virtual:windi.css';
+
+	// if you want to enable windi devtools
+	import { browser } from '$app/env';
+	if (browser) import('virtual:windi-devtools');
+</script>
+
+<main class="p-4 bg-gray-900 text-gray-100 h-screen">
+    <slot/>
+</main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import WindiCSS from 'vite-plugin-windicss'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,13 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+
+		vite: {
+			plugins: [
+				WindiCSS(),
+			],
+		},
 	}
 };
 

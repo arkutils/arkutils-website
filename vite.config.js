eb642dd55f3a87ef4c79d08cd5aa224833cbeb33
coldino <28345893+coldino@users.noreply.github.com>
2022-09-06 17:39:32 +0100

    fix: work around sveltekit#6462
    https://github.com/sveltejs/kit/issues/6462

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
     plugins: [
         sveltekit(),
-    ]
+    ],
+
+    ssr: {
+        noExternal: ['devalue'],
+    }
 };
 
 export default config;

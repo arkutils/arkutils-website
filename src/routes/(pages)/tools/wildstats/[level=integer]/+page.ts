ff03d1369cd8ff4ea5395bc447ab96dd231f5482
coldino <28345893+coldino@users.noreply.github.com>
2023-02-25 00:08:11 +0000

    feat: enable prerendering for most pages

--- src/routes/(pages)/tools/wildstats/[level=integer]/+page.ts
+++ src/routes/(pages)/tools/wildstats/[level=integer]/+page.ts
@@ -1,6 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
+
+export const prerender = false;
+
 export const load: PageLoad = async ({ params }) => {
     // Grab the params from the URL
     const { level } = params;
--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+// import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
@@ -12,10 +13,24 @@ const config = {
     ],
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            // fallback: 'index.html',
+        }),
         prerender: {
             crawl: true,
-            origin: "https://arkutils.netlify.app"
+            origin: "https://arkutils.netlify.app",
+            entries: [
+                "*", // all non-parameterized pages
+                "/tools/wildstats/150/6", // we prerender the common wildstats pages
+                "/tools/wildstats/150/7",
+                "/tools/wildstats/150/8",
+                "/tools/wildstats/217/6",
+                "/tools/wildstats/217/7",
+                "/tools/wildstats/217/8",
+                "/tools/wildstats/224/6",
+                "/tools/wildstats/224/7",
+                "/tools/wildstats/224/8",
+            ]
         }
     }
 };

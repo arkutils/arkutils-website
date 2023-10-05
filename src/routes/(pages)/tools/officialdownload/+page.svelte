79fc1b906c8df047b50546aacf136eb9a072d7aa
coldino <28345893+coldino@users.noreply.github.com>
2023-10-05 22:58:50 +0100

    feat(downloads): add final official downloads

--- src/routes/(pages)/tools/officialdownload/+page.svelte
+++ src/routes/(pages)/tools/officialdownload/+page.svelte
@@ -1,5 +1,8 @@
-<div class="flex flex-col justify-center">
-	<a class="btn btn-primary my-4 hover:text-primary-content" href="officialdownload/Aug2023">August 2023</a
-	>
-	<button class="btn btn-primary mb-4 btn-disabled" tabindex="-1" aria-disabled>September 2023</button>
+<div class="flex flex-col justify-center my-12">
+	<a class="btn btn-primary my-4 hover:text-primary-content" href="officialdownload/Aug2023">
+		August 2023
+	</a>
+	<a class="btn btn-primary my-4 hover:text-primary-content" href="officialdownload/Oct2023">
+		October 2023 (Final)
+	</a>
 </div>
---
+++ src/routes/(pages)/tools/officialdownload/Oct2023/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import ServerDownloads, { type Server } from '../ServerDownloads.svelte';
+	import serverData from './servers.json';
+
+	const servers = serverData.servers as Server[];
+</script>
+
+<h3 class="text-secondary text-3xl mt-8 mb-4">October 2023 (Final) List</h3>
+
+<ServerDownloads {servers} />
